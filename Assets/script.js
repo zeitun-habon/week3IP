4ea6435a9a118700804fe3a409b13744c7b982f0
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-05-17 21:50:51 +0300

    adding custom variabls

--- Assets/script.js
+++ Assets/script.js
@@ -9,3 +9,32 @@ const userScore = document.getElementById("user-score")
 const totalScore = document.getElementById("total-score")
 const questionText = document.getElementById("question-text")
 
+//custom variables
+let currentQuestion = 0;
+let score = 0;
+let question = [
+    {
+        question: "Is moringa the best bootcamp in Africa?",
+        answers:[
+           {option:"Of course!",answer:true},
+           {option:"No,not at all.", answer:false},
+            
+        ]
+    },
+    {
+        question:"Is HTML a programming language?",
+        answers:[
+            {option:"yes", answer: false},
+            {option:"No", answer : true},
+        ]
+    },
+
+    {
+     question:"what is CSS?",
+     answers:[
+         {option:"A styling language.", answer:true},
+         {option:"A scripting language.", answer:false},
+     ]
+    }
+
+]
