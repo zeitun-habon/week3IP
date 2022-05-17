62ae7213c7fad7bd281898e308e8af469e620fde
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-05-17 21:28:28 +0300

    adding the const variable

---
+++ Assets/script.js
@@ -0,0 +1,11 @@
+//const variabls
+const restartBtn = document.getElementById("restart")
+const prevBtn = document.getElementById("prev")
+const nextBtn = document.getElementById("next")
+const submitBtn = document.getElementById("submit")
+const trueBtn = document.getElementById("True")
+const falseBtn = document.getElementById("False")
+const userScore = document.getElementById("user-score")
+const totalScore = document.getElementById("total-score")
+const questionText = document.getElementById("question-text")
+
